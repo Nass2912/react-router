9ca30c249ada1f2cb4c94126d3972c03d63df151
Soobratty Anas <90127694+Nass2912@users.noreply.github.com>
2022-07-21 15:00:53 +0400

    Add files via upload

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
+import products from "../data";
 
 export const Products = () => {
+
   return (
       <section className='section'>
         <h2>products</h2>
         <Link to="/" className="btn">Back to home</Link>
+        <div className="products">
+          {
+            products.map(({id, name}) => {
+              return (
+                <article key={id}>
+                  <h5>{name}</h5>
+                  <Link to={`/products/${id}`} className="btn red">More Info</Link>
+                </article>
+              )
+            })
+          }
+        </div> 
       </section>
   );
 };
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,14 +1,20 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
 
 const SingleProduct = () => {
   const {productId} = useParams();
+  const product = products.find((producters) => {
+    return(producters.id === productId)
+  })
+  const {image, name} = product
+
   return (
-    <>
       <section className='section product'>
-        <h2>{productId}</h2>
+        <h2>{name}</h2>
+        <img src= {image} alt= {name} style={{display:"block",
+      marginBottom: "20px"}}></img>
+        <Link to='/products' className="btn">Back To Products</Link>
       </section>
-      <Link to='/products' className="btn">Back To Products</Link>
-    </>
   );
 };
 
